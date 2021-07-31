2875cb713920b8a2a7ffda533dccd5ee2dbcef60
scarr33 <84546977+scarr33@users.noreply.github.com>
2021-07-31 14:35:03 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("mySidenav").style.left = "0px";
 };
 
 const closeNav = () => {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.left = "-300px";
 };
