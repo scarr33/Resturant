6ef3a88c05554e896592f8cdc4f1ab983aaab48b
scarr33 <84546977+scarr33@users.noreply.github.com>
2021-08-14 02:45:55 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "300px";
   document.getElementById("mySidenav").style.left = "0px";
+  document.getElementById("overlay").style.zIndex = "4";
+  document.getElementById("overlay").style.background =
+    "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6))";
 };
 
 const closeNav = () => {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("mySidenav").style.left = "-300px";
+  document.getElementById("overlay").style.zIndex = "-5";
+  document.getElementById("overlay").style.background =
+    "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0))";
 };
